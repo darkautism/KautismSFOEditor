1f1f1ecc042f8129416dca96be5d0aad7b046f6f
kautism <darkautism@gmail.com>
2016-10-18 23:54:50 +0800

    Change version to v1.1.3

--- kautismSFOEditor/Properties/AssemblyInfo.cs
+++ kautismSFOEditor/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將組建和修訂編號
 // 指定為預設值:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.*")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.1.3.*")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
