1bfa20b2f3be0d8c295874ed82b94954391b2e40
kautism <darkautism@gmail.com>
2016-11-04 00:19:24 +0800

    Fix path error bug

--- kautismSFOEditor/Form1.cs
+++ kautismSFOEditor/Form1.cs
@@ -301,9 +301,9 @@ namespace kautismSFOEditor {
             String Extension_Icopath = @"\sfo.ico";//ICO的路徑
             try {
                 Registry.ClassesRoot.CreateSubKey("KautismSFOEditor").SetValue("", "SFO file editor");
-                // Registry.ClassesRoot.CreateSubKey("KautismSFOEditor\\DefaultIcon").SetValue("", System.Windows.Forms.Application.StartupPath + Extension_Icopath);
-                Registry.ClassesRoot.CreateSubKey("KautismSFOEditor\\DefaultIcon").SetValue("", Application.ExecutablePath + ",1");
-                Registry.ClassesRoot.CreateSubKey("KautismSFOEditor\\shell\\open\\command").SetValue("", Application.ExecutablePath + " %1", RegistryValueKind.ExpandString);
+                // Registry.ClassesRoot.CreateSubKey(Extension+"\\DefaultIcon").SetValue("", System.Windows.Forms.Application.StartupPath + Extension_Icopath);
+                Registry.ClassesRoot.CreateSubKey("KautismSFOEditor\\DefaultIcon").SetValue("", "\"" +Application.ExecutablePath + "\",1");
+                Registry.ClassesRoot.CreateSubKey("KautismSFOEditor\\Shell\\Open\\Command").SetValue("", "\"" + Application.ExecutablePath + "\" \"%1\"");
                 Registry.ClassesRoot.CreateSubKey(Extension).SetValue("", "KautismSFOEditor");
                 SHChangeNotify(0x8000000, 0x1000, IntPtr.Zero, IntPtr.Zero);
                 MessageBox.Show(Resources.strings.Setting_Finished);
@@ -319,90 +319,5 @@ namespace kautismSFOEditor {
         // --------------
         [System.Runtime.InteropServices.DllImport("Shell32.dll")]
         private static extern int SHChangeNotify(int eventId, int flags, IntPtr item1, IntPtr item2);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern IntPtr GetWindow(IntPtr hWnd, GetWindow_Cmd uCmd);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
-
-        enum GetWindow_Cmd : uint
-        {
-            GW_HWNDFIRST = 0,
-            GW_HWNDLAST = 1,
-            GW_HWNDNEXT = 2,
-            GW_HWNDPREV = 3,
-            GW_OWNER = 4,
-            GW_CHILD = 5,
-            GW_ENABLEDPOPUP = 6
-        }
-
-        private const int WM_COMMAND = 0x111;
-
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
-
-        private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
-
-        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
-        private static extern int GetWindowText(IntPtr hWnd, StringBuilder strText, int maxCount);
-
-        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
-        private static extern int GetWindowTextLength(IntPtr hWnd);
-
-        [DllImport("user32.dll")]
-        private static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
-
-        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
-
-
-        public static string GetWindowText(IntPtr hWnd) {
-            int size = GetWindowTextLength(hWnd);
-            if (size++ > 0) {
-                var builder = new StringBuilder(size);
-                GetWindowText(hWnd, builder, builder.Capacity);
-                return builder.ToString();
-            }
-
-            return String.Empty;
-        }
-
-        public static IEnumerable<IntPtr> FindWindowsWithClass(string className) {
-            IntPtr found = IntPtr.Zero;
-            List<IntPtr> windows = new List<IntPtr>();
-
-            EnumWindows(delegate (IntPtr wnd, IntPtr param)
-            {
-                StringBuilder cl = new StringBuilder(256);
-                GetClassName(wnd, cl, cl.Capacity);
-                if (cl.ToString() == className && (GetWindowText(wnd) == "" || GetWindowText(wnd) == null)) {
-                    windows.Add(wnd);
-                }
-                return true;
-            },
-                        IntPtr.Zero);
-
-            return windows;
-        }
-
-        static void ToggleDesktopIcons() {
-            var toggleDesktopCommand = new IntPtr(0x7402);
-            IntPtr hWnd = IntPtr.Zero;
-            if (Environment.OSVersion.Version.Major < 6 || Environment.OSVersion.Version.Minor < 2) //7 and -
-                hWnd = GetWindow(FindWindow("Progman", "Program Manager"), GetWindow_Cmd.GW_CHILD);
-            else {
-                var ptrs = FindWindowsWithClass("WorkerW");
-                int i = 0;
-                while (hWnd == IntPtr.Zero && i < ptrs.Count()) {
-                    hWnd = FindWindowEx(ptrs.ElementAt(i), IntPtr.Zero, "SHELLDLL_DefView", null);
-                    i++;
-                }
-            }
-            SendMessage(hWnd, WM_COMMAND, toggleDesktopCommand, IntPtr.Zero);
-        }
     }
 }
